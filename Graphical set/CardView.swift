1387e06f2b191352557f63643b39694d5acfd66f
Oklahomawhore <Oklahomawhore@user.noreply.github.com>
2018-08-12 21:33:59 +0800

    First Time Commit working from set-Game.

---
+++ Graphical set/CardView.swift
@@ -0,0 +1,21 @@
+//
+//  CardView.swift
+//  Graphical set
+//
+//  Created by Wangshu Zhu on 2018/8/12.
+//  Copyright © 2018年 Wangshu Zhu. All rights reserved.
+//
+
+import UIKit
+
+class CardView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
